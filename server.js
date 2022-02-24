b3d1d28c2cdf9f9cdf3d98efc8871d7336c24f17
ManukMelkonyan <myan.manuk@gmail.com>
2022-02-24 10:52:58 +0400

    Enabling data compression for faster responses

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const compression = require("compression");
 const express = require("express");
 const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(compression());
 
 app.use("/static", express.static(__dirname + "/build/static"));
 
